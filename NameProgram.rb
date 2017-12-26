1846f47b1913f6749e7b8ba08b24bc22ad36655d
Scott Mason <34557195+smason1983@users.noreply.github.com>
2017-12-26 11:57:04 -0500

    Add files via upload

---
+++ NameProgram.rb
@@ -0,0 +1,12 @@
+puts 'What is your favorite number?'
+favorite_number = gets.chomp
+new_number = favorite_number.to_i + 1
+puts 'Looks like ' + new_number.to_s +  ' is your new favorite number.'
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'What is your middle name?'
+middle_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+puts 'Hello ' + first_name + ' ' + middle_name + ' ' + last_name + '.'
+puts ''
